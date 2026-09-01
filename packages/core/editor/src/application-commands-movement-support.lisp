84a3272937f613218b40af133c46730e1e3214e7
takeokunn <bararararatty@gmail.com>
2026-09-01 20:12:13 +0900

    refactor: document movement command helpers

--- packages/core/editor/src/application-commands-movement-support.lisp
+++ packages/core/editor/src/application-commands-movement-support.lisp
@@ -5,29 +5,38 @@
 (in-package #:loom)
 
 (defun %move-point-to-position (buffer position)
+  "Move BUFFER point to POSITION."
   (buffer-set-point buffer
                     (buffer-position-line position)
                     (buffer-position-column position)))
 
 (defun %move-point-to-offset (buffer offset)
+  "Move BUFFER point to OFFSET."
   (%move-point-to-position buffer
                            (buffer-offset-position buffer offset)))
 
 (defmacro define-current-line-boundary-command (name column-form documentation)
-  `(defun ,name ()
-     ,documentation
-     (let ((buffer (%selected-buffer)))
-       (buffer-set-point buffer
-                         (buffer-point-line buffer)
-                         ,column-form))))
+  "Define NAME as a command moving point within its current line."
+  (let ((buffer (gensym "BUFFER-")))
+    `(defun ,name ()
+       ,documentation
+       (let ((,buffer (%selected-buffer)))
+         (symbol-macrolet ((buffer ,buffer))
+           (buffer-set-point ,buffer
+                             (buffer-point-line ,buffer)
+                             ,column-form))))))
 
 (defmacro define-buffer-boundary-command (name offset-form documentation)
-  `(defun ,name ()
-     ,documentation
-     (let ((buffer (%selected-buffer)))
-       (%move-point-to-offset buffer ,offset-form))))
+  "Define NAME as a command moving point to a buffer OFFSET."
+  (let ((buffer (gensym "BUFFER-")))
+    `(defun ,name ()
+       ,documentation
+       (let ((,buffer (%selected-buffer)))
+         (symbol-macrolet ((buffer ,buffer))
+           (%move-point-to-offset ,buffer ,offset-form))))))
 
 (defun %forward-char-once ()
+  "Move point forward by one character within the narrowing bounds."
   (let* ((buffer (%selected-buffer))
          (point (buffer-point-offset buffer))
          (end (buffer-narrow-end-offset buffer)))
@@ -35,6 +44,7 @@
       (%move-point-to-offset buffer (1+ point)))))
 
 (defun %backward-char-once ()
+  "Move point backward by one character within the narrowing bounds."
   (let* ((buffer (%selected-buffer))
          (point (buffer-point-offset buffer))
          (start (buffer-narrow-start-offset buffer)))
@@ -42,14 +52,17 @@
       (%move-point-to-offset buffer (1- point)))))
 
 (defun %next-line-once ()
+  "Move point down by one logical line."
   (let ((buffer (%selected-buffer)))
     (buffer-set-point buffer (1+ (buffer-point-line buffer)) (buffer-point-column buffer))))
 
 (defun %previous-line-once ()
+  "Move point up by one logical line."
   (let ((buffer (%selected-buffer)))
     (buffer-set-point buffer (1- (buffer-point-line buffer)) (buffer-point-column buffer))))
 
 (defmacro define-word-motion-helper (name offset-function)
+  "Define NAME as a one-step word motion helper using OFFSET-FUNCTION."
   (let ((buffer (gensym "BUFFER-"))
         (start (gensym "START-"))
         (offset (gensym "OFFSET-")))
