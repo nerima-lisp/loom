cdd5587234a2ddb97ec1cd097c68783a2a6c76c3
takeokunn <bararararatty@gmail.com>
2026-09-02 01:18:45 +0900

    refactor structural offset transformations

--- packages/core/editor/src/application-structural-editing.lisp
+++ packages/core/editor/src/application-structural-editing.lisp
@@ -120,20 +120,24 @@ to raise it out of and the operation would delete rather than promote."
           (list (list :delete end (- (1+ close) end))
                 (list :delete open (- start open))))))))
 
+(defun %structural-offset-after-insert (offset edit)
+  (let ((position (second edit)))
+    (if (<= position offset)
+        (+ offset (length (third edit)))
+        offset)))
+
+(defun %structural-offset-after-delete (offset edit)
+  (let* ((position (second edit))
+         (length (third edit))
+         (end (+ position length)))
+    (cond ((<= end offset) (- offset length))
+          ((< position offset) position)
+          (t offset))))
+
 (defun %structural-adjusted-offset-after-edit (offset edit)
   (ecase (first edit)
-    (:insert
-     (let ((position (second edit)))
-       (if (<= position offset)
-           (+ offset (length (third edit)))
-           offset)))
-    (:delete
-     (let* ((position (second edit))
-            (length (third edit))
-            (end (+ position length)))
-       (cond ((<= end offset) (- offset length))
-             ((< position offset) position)
-             (t offset))))))
+    (:insert (%structural-offset-after-insert offset edit))
+    (:delete (%structural-offset-after-delete offset edit))))
 
 (defun %structural-adjusted-offset (edits offset)
   "Return where OFFSET ends up once EDITS have been applied."
