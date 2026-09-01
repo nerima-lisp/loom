7549a615f3c8cf0bddad2b00f04f8f3506ae427f
takeokunn <bararararatty@gmail.com>
2026-09-02 08:20:07 +0900

    test yank-pop repeat count

--- t/integration/kill-yank-yank-test.lisp
+++ t/integration/kill-yank-yank-test.lisp
@@ -88,6 +88,24 @@
                 :to-equal '((1 . 2) (3 . 4))))))
 
   (it
+    "preserves the previous yank repeat count while rotating ranges"
+    (let ((*editor-state* (%fresh-editor-state "aXXbYYc")))
+      (let ((buffer (%selected-test-buffer)))
+        (setf (editor-state-kill-ring *editor-state*) '("Z")
+              (loom::editor-state-last-yank-buffer *editor-state*) buffer
+              (loom::editor-state-last-yank-start-offset *editor-state*) 1
+              (loom::editor-state-last-yank-end-offset *editor-state*) 6
+              (editor-state-last-yank-ranges *editor-state*)
+                '((1 . 3) (4 . 6))
+              (loom::editor-state-last-yank-ring-index *editor-state*) 0
+              (loom::editor-state-last-yank-repeat-count *editor-state*) 2)
+        (buffer-set-point buffer 0 6)
+        (loom::yank-pop)
+        (expect (buffer-text buffer) :to-equal "aZZbZZc")
+        (expect (loom::editor-state-last-yank-repeat-count *editor-state*)
+                :to-equal 2))))
+
+  (it
     "kills from point to end of line and yanks it back at a new position"
     (let ((*editor-state* (%fresh-editor-state "hello world")))
       (let ((buffer (%selected-test-buffer)))
