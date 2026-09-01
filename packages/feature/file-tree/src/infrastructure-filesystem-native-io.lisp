9acbecef827eeed224ad9f13c21b44f891deeeac
takeokunn <bararararatty@gmail.com>
2026-09-02 06:17:31 +0900

    refactor native file descriptor lifecycle

--- packages/feature/file-tree/src/infrastructure-filesystem-native-io.lisp
+++ packages/feature/file-tree/src/infrastructure-filesystem-native-io.lisp
@@ -4,42 +4,44 @@
 (in-package #:loom/feature/file-tree)
 
 #+sbcl
+(defmacro %with-native-file-descriptor ((fd path flags &optional mode) &body body)
+  `(let ((,fd (sb-posix:open (%native-namestring ,path) ,flags ,@(when mode `(,mode)))))
+     (unwind-protect
+          (progn
+            ,@body)
+       (when ,fd
+         (ignore-errors (sb-posix:close ,fd))))))
+
+#+sbcl
 (defun %native-read-file (path)
-  (let ((fd (sb-posix:open (%native-namestring path) sb-posix:o-rdonly)))
-    (unwind-protect
-         (let ((stream (sb-sys:make-fd-stream fd
-                                              :input t
-                                              :element-type 'character
-                                              :external-format :utf-8)))
-           (setf fd nil)
-           (unwind-protect
-                (let ((chunk (make-string 4096))
-                      (output (make-string-output-stream)))
-                  (loop for count = (read-sequence chunk stream)
-                        while (plusp count)
-                        do (write-string chunk output :end count))
-                  (get-output-stream-string output))
-             (close stream)))
-      (when fd
-        (ignore-errors (sb-posix:close fd))))))
+  (%with-native-file-descriptor (fd path sb-posix:o-rdonly)
+    (let ((stream (sb-sys:make-fd-stream fd
+                                         :input t
+                                         :element-type 'character
+                                         :external-format :utf-8)))
+      (setf fd nil)
+      (unwind-protect
+           (let ((chunk (make-string 4096))
+                 (output (make-string-output-stream)))
+             (loop for count = (read-sequence chunk stream)
+                   while (plusp count)
+                   do (write-string chunk output :end count))
+             (get-output-stream-string output))
+        (close stream)))))
 
 #+sbcl
 (defun %native-write-file (path content)
-  (let ((fd (sb-posix:open (%native-namestring path)
-                           (logior sb-posix:o-wronly
-                                   sb-posix:o-creat
-                                   sb-posix:o-trunc)
-                           #o666)))
-    (unwind-protect
-         (let ((stream (sb-sys:make-fd-stream fd
-                                              :output t
-                                              :element-type 'character
-                                              :external-format :utf-8)))
-           (setf fd nil)
-           (unwind-protect
-                (progn
-                  (write-string content stream)
-                  (finish-output stream))
-             (close stream)))
-      (when fd
-        (ignore-errors (sb-posix:close fd))))))
+  (%with-native-file-descriptor
+      (fd path (logior sb-posix:o-wronly
+                       sb-posix:o-creat
+                       sb-posix:o-trunc) #o666)
+    (let ((stream (sb-sys:make-fd-stream fd
+                                         :output t
+                                         :element-type 'character
+                                         :external-format :utf-8)))
+      (setf fd nil)
+      (unwind-protect
+           (progn
+             (write-string content stream)
+             (finish-output stream))
+        (close stream)))))
