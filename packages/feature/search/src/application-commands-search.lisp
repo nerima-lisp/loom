8d92666ca8d1a4a53c4b96476ae29b8d482a7a17
takeokunn <bararararatty@gmail.com>
2026-09-01 20:16:18 +0900

    refactor: make search command macro hygienic

--- packages/feature/search/src/application-commands-search.lisp
+++ packages/feature/search/src/application-commands-search.lisp
@@ -53,21 +53,25 @@ expansion."
           (minibuffer-message minibuffer "Not found")))))
 
 (defmacro %define-search-command (name prompt search-fn docstring)
-  `(defun ,name ()
+  (let ((buffer-var (gensym "BUFFER-"))
+        (span-var (gensym "SPAN-"))
+        (position-var (gensym "POSITION-")))
+    `(defun ,name ()
      ,docstring
      (with-prompts (minibuffer (editor-state-minibuffer *editor-state*)
                     :on-cancel (minibuffer-message minibuffer "Quit"))
          ((pattern ,prompt))
-       (let* ((buffer (%selected-buffer))
-              (span (,search-fn buffer pattern)))
-         (if span
-             (let ((position
-                     (buffer-offset-position buffer (buffer-span-start span))))
-               (buffer-set-point buffer
-                                 (buffer-position-line position)
-                                 (buffer-position-column position))
+       (let* ((,buffer-var (%selected-buffer))
+              (,span-var (,search-fn ,buffer-var pattern)))
+         (if ,span-var
+             (let ((,position-var
+                     (buffer-offset-position ,buffer-var
+                                              (buffer-span-start ,span-var))))
+               (buffer-set-point ,buffer-var
+                                 (buffer-position-line ,position-var)
+                                 (buffer-position-column ,position-var))
                (minibuffer-message minibuffer "Found"))
-             (minibuffer-message minibuffer "Not found"))))))
+             (minibuffer-message minibuffer "Not found")))))))
 
 (%define-search-command
  search-forward
