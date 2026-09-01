558bb2a21f1fe3c9f1275fa7359e79825751935f
takeokunn <bararararatty@gmail.com>
2026-09-01 11:25:43 +0900

    test: cover LSP completion transformations

--- t/integration/commands-lsp-navigation-test.lisp
+++ t/integration/commands-lsp-navigation-test.lisp
@@ -53,6 +53,39 @@
     (gethash "id" (%parse-lsp-json (car (last messages))))))
 
 (describe
+  "LSP completion data transformations"
+  (it-each
+      (("foo" 3 0)
+       ("foo-bar" 7 0)
+       ("foo +" 5 4)
+       ("" 0 0))
+      "finds the symbol prefix boundary in ~S at column ~A"
+      (content column expected)
+    (let ((buffer (make-buffer :initial-content content)))
+      (expect (loom/feature/lsp::%lsp-completion-prefix-column
+               buffer 0 column)
+              :to-equal expected)))
+
+  (it
+    "renders completion details without changing insertion text"
+    (let ((item (make-lsp-completion-item
+                 "map"
+                 :insert-text "mapcar"
+                 :detail "function")))
+      (expect (loom/feature/lsp::%lsp-completion-popup-items (list item))
+              :to-equal '(("map  function" . "mapcar")))))
+
+  (it
+    "uses the label when completion detail is absent"
+    (let ((item (make-lsp-completion-item
+                 "lambda"
+                 :insert-text nil
+                 :detail nil)))
+      (expect (loom/feature/lsp::%lsp-completion-popup-items (list item))
+              :to-equal '(("lambda" . "lambda")))))
+  )
+
+(describe
   "LSP completion at point"
   (it
     "reports the missing session without sending a request"
