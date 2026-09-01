9acbcf22aacc5271db6a5231c4aee74a21a819b3
takeokunn <bararararatty@gmail.com>
2026-09-02 03:20:14 +0900

    refactor format before save

--- packages/feature/format/src/application-commands-format.lisp
+++ packages/feature/format/src/application-commands-format.lisp
@@ -61,6 +61,27 @@ formatter command to have been configured first."
       (error "Set a format command before enabling format-on-save"))
     (setf (editor-state-format-on-save-p state) (not (null enabled)))))
 
+(defun %format-before-save-result (result state)
+  (when (and (not (shell-command-result-success-p result))
+             (editor-state-minibuffer state))
+    (minibuffer-message
+     (editor-state-minibuffer state)
+     (format nil "Format-on-save exited with code ~D"
+             (shell-command-result-exit-code result)))))
+
+(defun %format-before-save-command (buffer state)
+  (handler-case
+      (%format-before-save-result
+       (format-buffer-with-command
+        (editor-state-format-command state)
+        buffer)
+       state)
+    (error (condition)
+      (when (editor-state-minibuffer state)
+        (minibuffer-message
+         (editor-state-minibuffer state)
+         (%format-error-message "Format-on-save" condition))))))
+
 (defun format-before-save (buffer &optional (state *editor-state*))
   "Format BUFFER in place before STATE writes it to disk.
 
@@ -70,21 +91,7 @@ unsaveable."
   (when (and state
              (editor-state-format-on-save-p state)
              (editor-state-format-command state))
-    (handler-case
-        (let ((result (format-buffer-with-command
-                       (editor-state-format-command state)
-                       buffer)))
-          (unless (shell-command-result-success-p result)
-            (when (editor-state-minibuffer state)
-               (minibuffer-message
-               (editor-state-minibuffer state)
-               (format nil "Format-on-save exited with code ~D"
-                       (shell-command-result-exit-code result))))))
-      (error (condition)
-        (when (editor-state-minibuffer state)
-          (minibuffer-message
-           (editor-state-minibuffer state)
-           (%format-error-message "Format-on-save" condition))))))
+    (%format-before-save-command buffer state))
   buffer)
 
 (defun set-format-command-command ()
