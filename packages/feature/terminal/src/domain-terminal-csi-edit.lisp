815025800e91fa9d2c3aa43584e867a36883ee3b
takeokunn <bararararatty@gmail.com>
2026-09-02 18:09:58 +0900

    separate terminal edit dispatch data

--- packages/feature/terminal/src/domain-terminal-csi-edit.lisp
+++ packages/feature/terminal/src/domain-terminal-csi-edit.lisp
@@ -1,31 +1,34 @@
 (in-package #:loom/feature/terminal)
 
+(defun %terminal-screen-csi-edit-mode-handler (final)
+  (cdr (assoc final
+              '((#\J . %terminal-screen-clear-display)
+                (#\K . %terminal-screen-clear-line)))))
+
+(defun %terminal-screen-csi-edit-count-handler (final)
+  (cdr (assoc final
+              '((#\P . %terminal-screen-delete-characters)
+                (#\@ . %terminal-screen-insert-characters)
+                (#\X . %terminal-screen-erase-characters)
+                (#\L . %terminal-screen-insert-lines)
+                (#\M . %terminal-screen-delete-lines)
+                (#\S . %terminal-screen-scroll-up)
+                (#\T . %terminal-screen-scroll-down)))))
+
+(defun %terminal-screen-csi-edit-cursor-handler (final)
+  (cdr (assoc final
+              '((#\s . %terminal-screen-save-cursor)
+                (#\u . %terminal-screen-restore-cursor)))))
+
 (defun %terminal-screen-csi-edit (screen final parameters)
-  (let ((first (%terminal-screen-parameter parameters 0 1)))
-    (case final
-      (#\J
-       (%terminal-screen-clear-display screen
-                                       (first parameters)))
-      (#\K
-       (%terminal-screen-clear-line screen
-                                    (first parameters)))
-      (#\m nil)
-      (#\P
-       (%terminal-screen-delete-characters screen first))
-      (#\@
-       (%terminal-screen-insert-characters screen first))
-      (#\X
-       (%terminal-screen-erase-characters screen first))
-      (#\L
-       (%terminal-screen-insert-lines screen first))
-      (#\M
-       (%terminal-screen-delete-lines screen first))
-      (#\S
-       (%terminal-screen-scroll-up screen first))
-      (#\T
-       (%terminal-screen-scroll-down screen first))
-      (#\s
-       (%terminal-screen-save-cursor screen))
-      (#\u
-       (%terminal-screen-restore-cursor screen))
-      (#\r nil))))
+  (cond
+    ((%terminal-screen-csi-edit-mode-handler final)
+     (funcall (%terminal-screen-csi-edit-mode-handler final)
+              screen
+              (first parameters)))
+    ((%terminal-screen-csi-edit-count-handler final)
+     (funcall (%terminal-screen-csi-edit-count-handler final)
+              screen
+              (%terminal-screen-parameter parameters 0 1)))
+    ((%terminal-screen-csi-edit-cursor-handler final)
+     (funcall (%terminal-screen-csi-edit-cursor-handler final) screen))))
